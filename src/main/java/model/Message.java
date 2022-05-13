ad5ce397176703cdb8ad329d72cd0e613d386ec7
Jhorman Mera <1010159945@u.icesi.edu.co>
2022-05-13 16:56:06 -0500

    Model Started

---
+++ src/main/java/model/Message.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Message {
+    String type;
+    String msg;
+
+    public Message(String type, String msg) {
+        this.type = type;
+        this.msg = msg;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
