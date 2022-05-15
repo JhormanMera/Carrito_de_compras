6cf78e15c5f174e675b558b440daf4b437391c3a
JhormanMera <1010159945@u.icesi.edu.co>
2022-05-15 17:24:15 -0500

    UserProvider ended

--- src/main/java/provider/UserProvider.java
+++ src/main/java/provider/UserProvider.java
@@ -1,5 +1,55 @@
 package provider;
 
+import db.DbConnection;
+import entity.Order;
+import entity.Product;
+import entity.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class UserProvider {
 
+    public void addUser(User user) throws SQLException, ClassNotFoundException {
+        DbConnection conn = new DbConnection();
+        String sql="INSERT INTO usersA00369206 (natID) VALUES ('$USERID')";
+        sql= sql.replace("$USERID",user.getNatId());
+        conn.runQuery(sql);
+        conn.close();
+    }
+
+    public ArrayList<Order> getOrdersHistory(String natID) throws SQLException, ClassNotFoundException {
+
+        DbConnection conn = new DbConnection();
+
+        String sql = "SELECT ordersA00369206.id, ordersA00369206.creationDate, ordersA00369206.paymentDate, ordersA00369206.productsAmount, ordersA00369206.totalPrice FROM ordersA00369206 WHERE ordersA00369206.userID = '$ORDERID'" ;
+
+        sql = sql.replace("$ORDERID",natID);
+        ResultSet results =  conn.getData(sql);
+        ArrayList<Order> orders = new ArrayList<>();
+
+        while(results.next()){
+            int payed=0;
+            int id = results.getInt("id");
+
+            String creationDate  = results.getString("creationDate");
+
+            String paymentDate = results.getString("paymentDate");
+
+            int amount = results.getInt("productsAmount");
+
+            int price = results.getInt("totalPrice");
+
+            if(!paymentDate.equals("0")){
+                payed=1;
+            }
+
+            Order order = new Order(id, payed, Long.parseLong(creationDate), Long.parseLong(paymentDate), natID, amount, price);
+            orders.add(order);
+        }
+
+        return orders;
+
+    }
 }
